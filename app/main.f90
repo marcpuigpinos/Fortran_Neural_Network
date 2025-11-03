8ae74dca99bc16b6f8bec1cdd1d39fc8b97dd44d
Marc <marc@debian>
2025-11-04 00:07:59 +0100

    Redo everything

--- app/main.f90
+++ app/main.f90
@@ -4,22 +4,6 @@ program main
 
    implicit none
 
-   integer :: error, number_inputs, number_layers, number_outputs
-   real, allocatable, dimension(:) :: inputs
-
-   write (*, *) "Hello World!"
-
-   number_inputs = 3
-   number_outputs = 1
-   number_layers = 1
-   allocate (inputs(number_inputs))
-   inputs(1) = 1.0; inputs(2) = 2.0; inputs(3) = 0.5
-
-   error = net_init(number_inputs, number_outputs, number_layers)
-   error = net_add_layer(1, "sigmoid")
-   error = net_activate(inputs, number_outputs, number_inputs)
-   error = net_print()
-   error = net_dalloc()
-   error = net_print()
-
+   write(*,*) "Hello World!"
+   
 end program main
