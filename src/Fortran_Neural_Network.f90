0601634a99e96a39ce1869eec3d1ed96215bf0eb
Marc PB <marc.puigpinos@gmail.com>
2025-11-05 22:31:20 +0100

    Initialize layer, and public interface add layer created.

--- src/Fortran_Neural_Network.f90
+++ src/Fortran_Neural_Network.f90
@@ -7,8 +7,12 @@ module fnn
 
    private
 
+   ! public types
+   public :: fnn_error
+
    !  public interfaces
-   !public
+   public :: fnn_initialize_network, &
+             fnn_add_layer
 
    ! Constants
    integer, parameter :: FNN_MSG_BUFF = 256
@@ -24,16 +28,20 @@ module fnn
    ! Definition of the layer type
    type fnn_layer
        integer :: id, & ! Id of the layer
-           na, &  ! number of activations
-           nn     ! number of neurons
+                  na, & ! number of activations
+                  nn    ! number of neurons
+
+        real(kind=8), allocatable, dimension(:) :: activations ! Vector of activations
+        real(kind=8), allocatable, dimension(:) :: outputs ! Vector of outputs
+
    end type fnn_layer
    
    ! Global network parameters
    integer nl, & ! Number of layers
-       ll, & ! Layers array length
-       tnn   ! total number of neurons
+           ll, & ! Layers array length
+           tnn   ! total number of neurons
    
-   type(fnn_layer), allocatable :: layers(:) ! Array containing the layers of the network. 
+   type(fnn_layer), allocatable, target :: layers(:) ! Array containing the layers of the network. 
    
 contains
 
@@ -81,11 +89,12 @@ contains
         
     end function layer_arr_inc
     
-    type(fnn_error) function initialize_layer(layer, id, na, nn) result(error)
+    type(fnn_error) function initialize_layer(layer, id, na, nn, activations) result(error)
         type(fnn_layer), pointer :: layer
         integer, intent(in) :: id, & ! Layer id
-            na, & ! Number of activations of the layer
-            nn ! Number of neurons of the layer
+                               na, & ! Number of activations of the layer
+                               nn ! Number of neurons of the layer
+        real(kind=8), intent(in) :: activations(na)
 
         ! Initialize error
         call default_error(error)
@@ -94,7 +103,29 @@ contains
         layer%id = id
         layer%na = na
         layer%nn = nn
+
+        ! Initialize vector of activations
+        if (allocated(layer%activations)) deallocate(layer%activations, stat=error%code, errmsg=error%msg)
+        if (error%code /= 0) call exit_proc() ! Any error exit
+        allocate(layer%activations(na+1), stat=error%code, errmsg=error%msg)
+        if (error%code /= 0) call exit_proc() ! Any error exit
         
+        layer%activations(1) = 1d0
+        layer%activations(2:na+1) = activations
+
+        ! Initialize vector of outputs
+        if (allocated(layer%outputs)) deallocate(layer%outputs, stat=error%code, errmsg=error%msg)
+        if (error%code /= 0) call exit_proc() ! Any error exit
+        allocate(layer%outputs(nn), stat=error%code, errmsg=error%msg)
+        if (error%code /= 0) call exit_proc() ! Any error exit
+        layer%outputs = 0d0
+        
+    contains
+
+        subroutine exit_proc()
+            return
+        end subroutine exit_proc
+
     end function initialize_layer
 
 
@@ -116,7 +147,93 @@ contains
         if (error%code /= 0) return ! If any error return
         
         
-    end function fnn_initialize_network    
+    end function fnn_initialize_network   
+    
+    type(fnn_error) function fnn_add_layer(nn, na, activations) result(error)
+        integer, intent(in) :: nn ! Number of neurons of the layer
+        integer, optional, intent(in) :: na ! Number of activatoins. Optional: only needed when is first layer
+        real(kind=8), optional, intent(in) :: activations(:) ! Array of activations. Optional: only needed when is first layer.
+        type(fnn_layer), pointer :: layer, prev_layer
+
+        ! Initialize error
+        call default_error(error)
+
+        ! Nullify local pointers
+        call nullify_local_pointers()
+
+        ! Increase the number of layers by one
+        nl = nl + 1
+
+        ! If layer array length is equal to numbers of layers, increase the layers array.
+        if (nl == ll) then
+            error = layer_arr_inc()
+            if (error%code /= 0) then
+                call exit_proc() ! Exit the procedure
+            endif
+        endif
+
+        ! Increase the total number of neurons, by adding the number of neurons of this layer
+        tnn = tnn + nn
+
+        ! Check if we are first layer, and we have number of activations na argument given
+        ! Check if we are first layer, and we have activations array.
+        if (nl == 1 .and. (.not. present(na))) then
+            error%code = 1
+            error%msg = "fnn_add_layer: number of activations not given on first layer."
+            call exit_proc()
+
+        else if (nl > 1 .and. present(na)) then
+            error%code = 2
+            error%msg = "fnn_add_layer: number of activations is given in an intermidate layer."
+            call exit_proc()
+
+        end if        
+
+        ! Check if we are first layer, and we have activations array.
+        if (nl == 1 .and. (.not. present(activations))) then
+            error%code = 3
+            error%msg = "fnn_add_layer: activations array missing on first layer."
+            call exit_proc()
+
+        else if (nl > 1 .and. present(activations)) then
+            error%code = 4
+            error%msg = "fnn_add_layer: activations array is given in an intermidate layer."
+            call exit_proc()
+
+        end if
+
+        ! Check if activations array have the length.
+        if (size(activations) /= na) then
+            error%code = 5
+            error%msg = "fnn_add_layer: activations array length is different that number of activations"
+        endif
+
+        ! Reserve the memory for the layer
+        if (nl == 1) then
+            layer => layers(nl)
+            error = initialize_layer(layer, nl, na, nn, activations)
+            nullify(layer, prev_layer)
+        else
+            layer => layers(nl)
+            prev_layer => layers(nl-1)
+            error = initialize_layer(layer, nl, prev_layer%nn, nn, prev_layer%outputs)
+            nullify(layer, prev_layer)
+        endif
+
+        call exit_proc() ! Exit the procedure
+
+    contains
+
+        subroutine nullify_local_pointers()
+            nullify(layer, prev_layer)
+        end subroutine
+
+        subroutine exit_proc()
+            call nullify_local_pointers()
+            return
+        end subroutine
+
+    end function fnn_add_layer
         
    
 end module fnn
